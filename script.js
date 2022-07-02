c9fc0c56a9628e71c4677ad79a07a8c0dba27f19
BDK27 <briansam195@gmail.com>
2022-07-02 15:26:29 +0800

    home section completed

--- script.js
+++ script.js
@@ -28,7 +28,19 @@ closeBtn.onclick = () =>{
     searchForm.classList.remove('active');
 }
 
-
+let swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+});
 
 
 
