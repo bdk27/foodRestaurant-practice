b362e1b68d8c888fccfb0eac210fbc9938a3b230
BDK27 <briansam195@gmail.com>
2022-07-02 12:47:48 +0800

    header completed

---
+++ script.js
@@ -0,0 +1,38 @@
+
+/* header section */
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+/* search form  */
+
+let searchIcon = document.querySelector('#search-icon');
+let searchForm = document.querySelector('#search-form');
+let closeBtn = document.querySelector('#close');
+
+searchIcon.onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+closeBtn.onclick = () =>{
+    searchForm.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+
